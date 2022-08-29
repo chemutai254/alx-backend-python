219c319a21f74ab200f34a4ac9de1066f2c8388d
chemutai254 <nancy.chemutai254>
2022-08-29 03:54:39 -0700

    Parameterzation of unit tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,17 +14,15 @@ class TestAccessNestedMap(unittest.TestCase):
         nested_map={"a": {"b": 2}}, path=("a",),
         nested_map={"a": {"b": 2}}, path=("a", "b"),
     ])
-    def test_access_nested_map(self, a, b, path):
+    def test_access_nested_map(self, nested_map, message):
         """
         Method testing to establish whether it returns
         what its suppossed to
         """
-        assert_equal(a, b, path)
-
     @parametized.expand([
         nested_map={"a": 1}, path=("a",),
         nested_map={"a": {"b": 2}}, path=("a",),
         nested_map={"a": {"b": 2}}, path=("a", "b"),
     ])
-    def test_access_nested_map(self, a, b, path):
-        assert_equal(a, b, path)
+    message = "Successfully returns the expected output"
+    self.assert_equal(nested_map, message)
